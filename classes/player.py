6d800f991f51a338265b9d046fee099c2bf78206
alves-Moises <moisesinho_ra@hotmail.com>
2022-07-25 18:31:02 -0300

    class player

--- classes/player.py
+++ classes/player.py
@@ -1,16 +1,12 @@
-from turtle import pos
-
-
-class Jogador():
-    def __init__(self, name, nacionality, height, better_foot, position, shirt_number, site_score, team_history, player_value, total_goals, season_goals, total_mathes, season_matches, total_assists, season_assists):
+class Player():
+    def __init__(self, player_id, name, nationality, birthdate, image_path):
+        self.id = player_id
         self.name = name
-        self.nacionality = nacionality
-        self.height = height
-        self.better_foot = better_foot
-        self.position = position
-        self.shirt_number = shirt_number = shirt_number
-        
-        pass
+        self.nationality = nationality
+        self.birthdate = birthdate
+        self.image_path = image_path
 
-        #TODO: fix self atributes
+
+        
+
         
