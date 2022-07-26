4b1a03ec2a73855e479eeec43bdf28359fb36261
alves-Moises <moisesinho_ra@hotmail.com>
2022-07-25 23:23:01 -0300

    update: player_controller.py

--- controllers/player_controller.py
+++ controllers/player_controller.py
@@ -1,7 +1,8 @@
-from pkgutil import get_data
 from api_requests.api_requests import *
+
 import json
 import classes.player
+
 api_requests = ApiRequests()
 
 def search_player(name):
@@ -14,8 +15,11 @@ def search_player(name):
 
     #players objects
     for player in player_data:
-        player_list.append(classes.player.Player(player['player_id'], player['common_name'], player['nationality'], player['birthdate'], player['image_path']))
+        if name in player['fullname']:
+            player_list.append(classes.player.Player(player['player_id'], player['fullname'], player['nationality'], player['birthdate'], player['image_path']))
 
+    
+    if player_list == []: return False
     return player_list
     # print(get_data(player_data))
    
