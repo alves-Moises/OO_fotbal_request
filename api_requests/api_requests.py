113824cf3ed3e7d8b9cc1d2ca2bcbec0d2b1c35b
alves-Moises <moisesinho_ra@hotmail.com>
2022-07-26 23:43:08 -0300

    comments api requests TODO

--- api_requests/api_requests.py
+++ api_requests/api_requests.py
@@ -26,3 +26,20 @@ class ApiRequests():
         response = requests.request("GET", url=name_player_url)
 
         return response.text
+
+
+
+
+# '''
+# todas as ligas
+# f"https://soccer.sportmonks.com/api/v2.0/leagues?api_token={api_token}"
+# {"data":[{"id":271,"active":true,"type":"domestic","legacy_id":43,"country_id":320,"logo_path":"https:\/\/cdn.sportmonks.com\/images\/soccer\/leagues\/271.png","name":"Superliga","is_cup":false,"is_friendly":false,"current_season_id":19686,"current_round_id":273989,"current_stage_id":77457696,"live_standings":true,"coverage":{"predictions":true,"topscorer_goals":true,"topscorer_assists":true,"topscorer_cards":true}},{"id":501,"active":true,"type":"domestic","legacy_id":66,"country_id":1161,"logo_path":"https:\/\/cdn.sportmonks.com\/images\/soccer\/leagues\/501.png","name":"Premiership","is_cup":false,"is_friendly":false,"current_season_id":19735,"current_round_id":null,"current_stage_id":null,"live_standings":true,"coverage":{"predictions":true,"topscorer_goals":true,"topscorer_assists":true,"topscorer_cards":true}},{"id":513,"active":true,"type":"domestic","legacy_id":null,"country_id":1161,"logo_path":"https:\/\/cdn.sportmonks.com\/images\/soccer\/leagues\/1\/513.png","name":"Premiership Play-Offs","is_cup":false,"is_friendly":false,"current_season_id":19611,"current_round_id":null,"current_stage_id":null,"live_standings":false,"coverage":{"predictions":true,"topscorer_goals":true,"topscorer_assists":true,"topscorer_cards":true}},{"id":1659,"active":true,"type":"domestic","legacy_id":null,"country_id":320,"logo_path":"https:\/\/cdn.sportmonks.com\/images\/\/soccer\/leagues\/27\/1659.png","name":"Superliga Play-offs","is_cup":false,"is_friendly":false,"current_season_id":18101,"current_round_id":null,"current_stage_id":null,"live_standings":false,"coverage":{"predictions":true,"topscorer_goals":true,"topscorer_assists":true,"topscorer_cards":true}}],"meta":{"plans":[{"name":"Football Free Plan","features":"Standard","request_limit":"180,60","sport":"Soccer"}],"sports":[{"id":1,"name":"Soccer","current":true}],"pagination":{"total":4,"count":4,"per_page":100,"current_page":1,"total_pages":1,"links":{}}}}
+# '''
+
+
+'''
+estatisticas do time
+
+id_do_time = 3468
+name_player_url = f"https://soccer.sportmonks.com/api/v2.0/teams/{id_do_time}?api_token={api_token}&include=stats"
+'''
