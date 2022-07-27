d0ad9c76e98cc2908f9f7107fe77cc825f7224bd
alves-Moises <moisesinho_ra@hotmail.com>
2022-07-27 15:13:03 -0300

    new controller: request_league_by_name

--- controllers/league_controller.py
+++ controllers/league_controller.py
@@ -17,3 +17,13 @@ def request_league():
         league_list.append(classes.league.League(item['id'], item['name'], item['active'], item['type'], item['country_id']))
 
     return league_list
+
+def request_league_by_name(name):
+    league_list = request_league()
+
+    new_list = list()
+    
+    for league in league_list:
+        if name in league.get_name(): new_list.append(league)
+
+    return new_list
